ab48b05d05bd2110853ba650621fc043c9a83f4d
joaolucasbatista <131275188+joaolucasbatista@users.noreply.github.com>
2023-05-09 07:46:32 -0300

    Add files via upload

---
+++ web2/index.js
@@ -0,0 +1,16 @@
+alert('Bom dia...')
+
+function Soma(){
+    n1 = Number(prompt('Digite um numero:'))
+    n2 = Number(prompt('Digite seu segundoi numero'))
+
+    m = (n1+ n2)
+
+    alert('resposta é'+m)
+} function mul(){
+    n1 = Number(prompt('Digite um numero..'))
+    n2 = Number(prmpt('Diqwgite o segundo numero'))
+
+    m = (n1 * n2)
+
+}
